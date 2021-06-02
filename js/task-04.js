e64ea7660603e4e46c288fa9408e99d23d843071
OlhaDudchenko <dudchenko.olha92@gmail.com>
2021-06-02 21:53:38 +0300

    update.task-04

--- js/task-04.js
+++ js/task-04.js
@@ -3,32 +3,25 @@
 //1) Создай переменную counterValue в которой будет хранится текущее значение счетчика.
 //2) Создай функции increment и decrement для увеличения и уменьшения значения счетчика
 //3) Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
+const refs = {
+ValueEl:document.querySelector('#value'),
+removeDecrement:document.querySelector('#counter').firstElementChild,
+addIncrement:document.querySelector('#counter').lastElementChild,
+};
+
+refs.addIncrement.addEventListener('click', onButtonClick);
+refs.removeDecrement.addEventListener('click', (onButtonClick2));
 let counterValue = 0;
 
-const ValueEl = document.querySelector('#value');
-// console.log(ValueEl.textContent);
-
-
-const removeDecrement = document.querySelector('#counter').firstElementChild;
-// console.log(removeDecrement);
-const addIncrement = document.querySelector('#counter').lastElementChild;
-// console.log(addIncrement);
-
-
-
-
 function onButtonClick() {
-   
-    counterValue += 1;
-    ValueEl.textContent = counterValue;
-    }
+   counterValue += 1;
+   refs.ValueEl.textContent = counterValue;
+};
 
-addIncrement.addEventListener('click', onButtonClick);
 
 function onButtonClick2() {
-   
     counterValue -= 1;
-    ValueEl.textContent = counterValue;
-}
-removeDecrement.addEventListener('click', (onButtonClick2));
+    refs.ValueEl.textContent = counterValue;
+};
+
 
