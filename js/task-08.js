3ab8e9805ab0b8686ce1fac505a055bea514ef76
OlhaDudchenko <dudchenko.olha92@gmail.com>
2021-06-02 18:18:45 +0300

    add task-08

---
+++ js/task-08.js
@@ -0,0 +1,55 @@
+// Напиши скрипт создания и очистки коллекции элементов. 
+// Пользователь вводит количество элементов в input и нажимает кнопку Создать,
+// после чего рендерится коллекция.
+// При нажатии на кнопку Очистить, коллекция элементов очищается.
+// Создай функцию createBoxes(amount), которая принимает 1 параметр amount - число.
+// Функция создает столько div, сколько указано в amount и добавляет их в div#boxes.
+// Каждый созданный div:
+// Имеет случайный rgb цвет фона
+// Размеры самого первого div - 30px на 30px
+// Каждый следующий div после первого, должен быть шире и выше предыдущего на 10px
+// Создай функцию destroyBoxes(), которая очищает div#boxes.
+const refs = {
+     input:document.querySelector('input'),
+     btnRender: document.querySelector('[data-action="render"]'),
+     btnDestroy: document.querySelector('[data-action="destroy"]'),
+     divContainer:document.querySelector('#boxes'),
+}
+
+refs.btnRender.addEventListener('click', getAmount);
+refs.btnDestroy.addEventListener('click', destroyOnClick);
+
+function getAmount() {
+const amount = refs.input.value;
+    
+ createBoxes(amount);
+};
+
+function createBoxes(amount) {
+const defaultSize = 30;
+
+for (let i = 0; i < amount; i += 1){
+
+    let size = defaultSize + i*10 ;
+    const  divEl = document.createElement('div');
+    divEl.style.cssText = `width:${size}px;height:${size}px;background-color:rgb(${randomRgb(0,255)},${randomRgb(0,255)},${randomRgb(0,255)});`
+    refs.divContainer.append(divEl)
+    };
+    
+   };
+
+function destroyOnClick(){
+    refs.input.value = '';
+    refs.divContainer.innerHTML = '';
+};
+
+function randomRgb(min,max) {
+   return Math.round(Math.random() * (max - min) + min);
+};
+
+
+
+
+
+
+
